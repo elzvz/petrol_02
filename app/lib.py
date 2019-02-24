78e3a35d2292d7f77a824c50e8c808696632f852
Zukhra <elzvz@mail.ru>
2019-02-24 12:48:36 +0300

    added exactly correct code

--- app/lib.py
+++ app/lib.py
@@ -1,22 +1,17 @@
 def petrol_consumption_in_litre_per_100_km (petrol_filling_in_litre, driving_on_this_petrol):
     """
-    >>>petrol_consumption_in_litre_per_100_km(5, 100)
+    >>> petrol_consumption_in_litre_per_100_km (5, 100)
     5.0
 
-    :param petrol_filling_in_litre:
-    :param driving_on_this_petrol:
-    :return:
     """
+    data = petrol_consumption_in_litre_per_100_km
     data = petrol_filling_in_litre / driving_on_this_petrol * 100
     return data
-def max_distance_on_this_petrol (petrol_volume, petrol_consumption_in_litre_per_100_km):
+def max_distance_on_this_petrol (petrol_volume, data):
     """
     >>> max_distance_on_this_petrol(10, 5)
     200.0
 
-    :param petrol_volume:
-    :param petrol_consumption_in_litre_per_100_km:
-    :return:
     """
-    result = petrol_volume / petrol_consumption_in_litre_per_100_km *100
+    result = petrol_volume / data *100
     return result
--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
 from app.lib import petrol_consumption_in_litre_per_100_km, max_distance_on_this_petrol
+if __name__ == '__main__':
+    petrol_filling_in_litre = int (input('Сколько бензина заправили? '))
+    print(petrol_filling_in_litre)
+    print(type(petrol_filling_in_litre))
 
-petrol_filling_in_litre = int (input('Сколько бензина заправили? '))
-print(petrol_filling_in_litre)
-print(type(petrol_filling_in_litre))
+    driving_on_this_petrol = int (input('Сколько проехали после заправки? '))
+    print (driving_on_this_petrol)
+    print (type (driving_on_this_petrol))
 
-driving_on_this_petrol = int (input('Сколько проехали после заправки? '))
-print (driving_on_this_petrol)
-print (type (driving_on_this_petrol))
+    data = petrol_consumption_in_litre_per_100_km (petrol_filling_in_litre, driving_on_this_petrol)
+    data = petrol_filling_in_litre / driving_on_this_petrol * 100
+    print(data)
+    print(petrol_consumption_in_litre_per_100_km)
 
-data = petrol_consumption_in_litre_per_100_km (petrol_filling_in_litre, driving_on_this_petrol)
-data = petrol_filling_in_litre / driving_on_this_petrol * 100
-print(data)
-print(petrol_consumption_in_litre_per_100_km)
+    petrol_volume = int (input('Сколько залили в бак потом? '))
+    print(petrol_volume)
+    print(type(petrol_volume))
 
-petrol_volume = int (input('Сколько залили в бак потом? '))
-print(petrol_volume)
-print(type(petrol_volume))
-
-max_distance_on_this_petrol (petrol_volume, data)
-value= petrol_volume / data *100
-print(value)
-print(max_distance_on_this_petrol)
+    max_distance_on_this_petrol (petrol_volume, data)
+    value= petrol_volume / data *100
+    print(value)
+    print(max_distance_on_this_petrol)
